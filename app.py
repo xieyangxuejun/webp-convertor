48bff3359b16cff3ce87c85d62507a2e71ac2523
xieyangxuejun <xieyangxuejun@gmail.com>
2018-06-20 00:11:29 +0800

    init

---
+++ app.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+import os
+from flask import Flask, request, \
+    redirect, url_for, render_template, \
+    send_from_directory
+from werkzeug import secure_filename
+from time import time
+
+CURRENT_DIRNAME = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = CURRENT_DIRNAME + '\\static'
+ALLOWED_EXTENSIONS = {'zip', 'png', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    cmd_str = CURRENT_DIRNAME + '\\bin\\windows\\img2webp.exe -lossy '
+    if request.method == 'POST':
+        upload_files = request.files.getlist('file[]')
+
+        # create new dir
+        time_stamp = str(int(time()))
+        files_dir = CURRENT_DIRNAME + '\\static\\' + time_stamp
+        print('files_dir====>' + files_dir)
+        os.mkdir(files_dir)
+
+        # single convert
+        if len(upload_files) == 1:
+            file = upload_files[0]
+            file_out_path = files_dir + '\\' + file.filename.split('.')[0] + '.webp'
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(files_dir, filename))
+                abspath = files_dir + '\\' + filename
+                cmd_str += abspath + ' '
+            cmd_str += '-o ' + file_out_path
+            print(cmd_str)
+            os.system(cmd_str)
+        else:
+            # out path
+            file_out_path = files_dir + '\\anim.webp'
+            file_paths = []
+            for file in upload_files:
+                if file and allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(files_dir, filename))
+                    abspath = files_dir + '\\' + filename
+                    cmd_str += abspath + ' '
+                    file_paths.append(abspath)
+            cmd_str += '-o ' + file_out_path
+            print(cmd_str)
+            os.system(cmd_str)
+            # for abspath in file_paths:
+            #     os.remove(abspath)
+    return render_template('upload.html')
+
+
+@app.route('/<filename>')
+def show(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True
+    )
