466b47a1bd57f610f7871baf41d2b42482e5fd3f
xieyangxuejun <xieyangxuejun@gmail.com>
2018-06-20 10:57:47 +0800

    Add mac support

--- app.py
+++ app.py
@@ -3,12 +3,13 @@
 import os
 from flask import Flask, request, \
     redirect, url_for, render_template, \
-    send_from_directory
+    send_from_directory, abort
 from werkzeug import secure_filename
 from time import time
 
+SEP = os.sep
 CURRENT_DIRNAME = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLDER = CURRENT_DIRNAME + '\\static'
+UPLOAD_FOLDER = CURRENT_DIRNAME + SEP + 'static'
 ALLOWED_EXTENSIONS = {'zip', 'png', 'gif'}
 
 app = Flask(__name__)
@@ -22,37 +23,39 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload():
-    cmd_str = CURRENT_DIRNAME + '\\bin\\windows\\img2webp.exe -lossy '
+    # replace windows or mac
+    cmd_str = CURRENT_DIRNAME + (str.replace('/bin/%s/img2webp -lossy ', '/', SEP) % 'mac')
     if request.method == 'POST':
         upload_files = request.files.getlist('file[]')
-
         # create new dir
         time_stamp = str(int(time()))
-        files_dir = CURRENT_DIRNAME + '\\static\\' + time_stamp
+        # mac
+        files_dir = CURRENT_DIRNAME + SEP + 'static' + SEP + time_stamp
         print('files_dir====>' + files_dir)
         os.mkdir(files_dir)
 
         # single convert
         if len(upload_files) == 1:
             file = upload_files[0]
-            file_out_path = files_dir + '\\' + file.filename.split('.')[0] + '.webp'
+            file_out_path = files_dir + SEP + file.filename.split('.')[0] + '.webp'
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(files_dir, filename))
-                abspath = files_dir + '\\' + filename
+                abspath = files_dir + SEP + filename
                 cmd_str += abspath + ' '
             cmd_str += '-o ' + file_out_path
             print(cmd_str)
             os.system(cmd_str)
         else:
             # out path
-            file_out_path = files_dir + '\\anim.webp'
+            file_name_out = '%s.webp' % time_stamp
+            file_out_path = CURRENT_DIRNAME + SEP + 'static' + SEP + file_name_out
             file_paths = []
             for file in upload_files:
                 if file and allowed_file(file.filename):
                     filename = secure_filename(file.filename)
                     file.save(os.path.join(files_dir, filename))
-                    abspath = files_dir + '\\' + filename
+                    abspath = files_dir + SEP + filename
                     cmd_str += abspath + ' '
                     file_paths.append(abspath)
             cmd_str += '-o ' + file_out_path
@@ -60,15 +63,18 @@ def upload():
             os.system(cmd_str)
             # for abspath in file_paths:
             #     os.remove(abspath)
+            file_url = url_for('show', name=file_name_out)
+            print(file_url)
+            return render_template('upload.html') + '<br><img src=' + file_url + '>'
     return render_template('upload.html')
 
-
-@app.route('/<filename>')
-def show(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
+@app.route('/show/<name>')
+def show(name):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], name)
 
 if __name__ == '__main__':
     app.run(
+        host='0.0.0.0',
+        port='5001',
         debug=True
     )
